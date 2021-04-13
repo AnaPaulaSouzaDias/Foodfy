1e295fbe50aa41fc37580c646dc4d4ffe474a7de
Ana Paula Souza Dias <anapaula_sdias@hotmail.com>
2021-04-13 19:16:58 -0300

    final do front-end

--- layouts/specs/scripts.js
+++ layouts/specs/scripts.js
@@ -4,12 +4,12 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
   card.addEventListener("click", function () {
+    const video_id = card.getAttribute("id");
     modalOverlay.classList.add('active')
-    modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${video_id}`;
+    modalOverlay.querySelector("img").src = `../assets/${video_id}`;
   })
 }
 
 const closeModal = document.querySelector('.close-modal').addEventListener("click", function () {
   modalOverlay.classList.remove("active")
-  modalOverlay.querySelector("iframe").src = ""
 })
